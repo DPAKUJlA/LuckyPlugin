57ae0ba121079a692df92f406597372fd3aced4d
Denisska008 <eduardkravcenko723@gmail.com>
2021-09-27 17:44:17 +0300

    Update code

--- src/main/java/org/lucky/Main.java
+++ src/main/java/org/lucky/Main.java
@@ -1,14 +1,6 @@
 package org.lucky;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Spider;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
