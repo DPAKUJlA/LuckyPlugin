79c000bb37f960d79404ce976da78e98dbdd7220
Denisska008 <eduardkravcenko723@gmail.com>
2021-09-27 17:43:18 +0300

    Update code

--- src/main/java/org/lucky/Main.java
+++ src/main/java/org/lucky/Main.java
@@ -11,7 +11,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin implements Listener {
+public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
